52aae00d453182b81da50eb0f245f77c6a8ef2b7
fabiberlin <fabi.in.berlin@googlemail.com>
2017-01-08 13:09:20 +0100

    read moepPeter!

--- app/src/main/java/com/happening/poc/poc_happening/fragment/Bt4Controls.java
+++ app/src/main/java/com/happening/poc/poc_happening/fragment/Bt4Controls.java
@@ -2,7 +2,9 @@ package com.happening.poc.poc_happening.fragment;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattServer;
 import android.bluetooth.BluetoothGattServerCallback;
 import android.bluetooth.BluetoothGattService;
@@ -36,7 +38,6 @@ import android.widget.TextView;
 import com.happening.poc.poc_happening.R;
 import com.happening.poc.poc_happening.adapter.DeviceListAdapter;
 import com.happening.poc.poc_happening.adapter.DeviceModel;
-import com.happening.poc.poc_happening.bluetooth.GattServerCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -305,23 +306,75 @@ public class Bt4Controls extends Fragment {
         gattCharacteristic.setValue("moep".getBytes());
         gattService.addCharacteristic(gattCharacteristic);
 
-        getBluetoothGattServer().addService(gattService);
+        mBluetoothGattServer = mBluetoothManager.openGattServer(rootView.getContext(), mGattServerCallback);
+
+        mBluetoothGattServer.addService(gattService);
     }
 
     private void stopGattServer() {
         if (mBluetoothGattServer != null) {
             Snackbar.make(rootView, "Stop Gatt-Server", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            getBluetoothGattServer().close();
+            mBluetoothGattServer.close();
         } else {
             Snackbar.make(rootView, "Nuttin to close", Snackbar.LENGTH_LONG).setAction("Action", null).show();
         }
 
     }
 
-    public BluetoothGattServer getBluetoothGattServer() {
-        if (mBluetoothGattServer == null) {
-            mBluetoothGattServer = mBluetoothManager.openGattServer(rootView.getContext(), mGattServerCallback);
+    //region GattServerCallback
+
+    public class GattServerCallback extends BluetoothGattServerCallback {
+
+        @Override
+        public void onCharacteristicReadRequest(BluetoothDevice device, int requestId, int offset, BluetoothGattCharacteristic characteristic) {
+            Log.d("CHAR_READ", "character read " + new String(characteristic.getValue()));
+            Bt4Controls controls = Bt4Controls.getInstance();
+
+            Log.d("CHAR_READ", "server " + mBluetoothGattServer);
+            if (mBluetoothGattServer != null)
+                mBluetoothGattServer.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, offset, (characteristic.getStringValue(0)+"Peter").getBytes());
         }
-        return mBluetoothGattServer;
+
+        @Override
+        public void onDescriptorReadRequest(BluetoothDevice device, int requestId, int offset, BluetoothGattDescriptor descriptor) {
+            Log.d("DESC_READ", "descriptor read " + new String(descriptor.getValue()));
+        }
+
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            Log.d("CONN_CHANGE", "state changed to " + newState);
+        }
+
+        @Override
+        public void onServiceAdded(int status, BluetoothGattService service) {
+            Log.d("SERVICE_ADD", "service added " + status + " " + service.getUuid().toString());
+        }
+
+        @Override
+        public void onCharacteristicWriteRequest(BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
+            Log.d("CHAR_WRITE", "device: " + device.getAddress() + " preparedWrite: " + preparedWrite + " responseNeeded: " + responseNeeded);
+        }
+
+        @Override
+        public void onDescriptorWriteRequest(BluetoothDevice device, int requestId, BluetoothGattDescriptor descriptor, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
+            Log.d("DESC_WRITE", "device: " + device.getAddress() + " preparedWrite: " + preparedWrite + " responseNeeded: " + responseNeeded);
+        }
+
+        @Override
+        public void onExecuteWrite(BluetoothDevice device, int requestId, boolean execute) {
+            Log.d("EXECUTE", "device: " + device.getAddress() + " execute: " + execute);
+        }
+
+        @Override
+        public void onNotificationSent(BluetoothDevice device, int status) {
+            Log.d("NOTIFICATION", "device: " + device.getAddress() + " status: " + status);
+        }
+
+        @Override
+        public void onMtuChanged(BluetoothDevice device, int mtu) {
+            Log.d("MTU_CHANGE", "device: " + device.getAddress() + " mtu: " + mtu);
+        }
     }
+
+    //endregion
 }
