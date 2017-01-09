90c886cc18e2a039d513e1758646ca23daa50477
Kai Schulz <mail@kaischulz.com>
2017-01-09 15:33:48 +0100

    use Layer class in Bt4Service
    reduce code duplication

--- app/src/main/java/com/happening/poc/poc_happening/service/Bluetooth4Service.java
+++ app/src/main/java/com/happening/poc/poc_happening/service/Bluetooth4Service.java
@@ -1,44 +1,31 @@
 package com.happening.poc.poc_happening.service;
 
 import android.app.Service;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.le.AdvertiseCallback;
-import android.bluetooth.le.AdvertiseData;
-import android.bluetooth.le.AdvertiseSettings;
-import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.os.ParcelUuid;
 import android.util.Log;
 
-import java.util.Random;
+import com.happening.poc.poc_happening.bluetooth.Layer;
 
 public class Bluetooth4Service extends Service {
 
-    private static final String HAPPENING_SERVICE_UUID = "11111111-1337-1337-1337-000000000000";
-    public static final ParcelUuid parcelUuid = ParcelUuid.fromString(HAPPENING_SERVICE_UUID);
-    private static final String TAG =  "TAGTAG";
-    // Binder given to clients
-    private final IBinder mBinder = new LocalBinder();
+    private Layer bt4Layer = null;
+
     /**
      * interface for clients that bind
      */
-    private final Random mGenerator = new Random();
+    private final IBinder mBinder = new LocalBinder();
+
     /**
      * indicates how to behave if the service is killed
      */
     int mStartMode = START_STICKY;
+
     /**
      * indicates whether onRebind should be used
      */
     boolean mAllowRebind = false;
-    private BluetoothManager mBluetoothManager = null;
-    private BluetoothAdapter mBluetoothAdapter = null;
-    private BluetoothLeAdvertiser mBluetoothLeAdvertiser = null;
-    private AdvertiseCallback mAdvertiseCallback = null;
-    private int number = -1;
 
     /**
      * Called when the service is being created.
@@ -46,29 +33,9 @@ public class Bluetooth4Service extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(this.getClass().getSimpleName(), "onCreate");
+        Log.d(this.getClass().getSimpleName(), "onCreate " + this.toString());
 
-        this.mBluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-        this.mBluetoothAdapter = mBluetoothManager.getAdapter();
-
-        this.mBluetoothLeAdvertiser = mBluetoothAdapter.getBluetoothLeAdvertiser();
-
-        this.mAdvertiseCallback = new AdvertiseCallback() {
-            @Override
-            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
-                super.onStartSuccess(settingsInEffect);
-                Log.d(this.getClass().getSimpleName(), "advertising started");
-            }
-
-            @Override
-            public void onStartFailure(int errorCode) {
-                super.onStartFailure(errorCode);
-                Log.d(this.getClass().getSimpleName(), "advertising error " + errorCode);
-                mBluetoothLeAdvertiser.stopAdvertising(mAdvertiseCallback);
-                startAdvertiser();
-            }
-
-        };
+        bt4Layer = Layer.getInstance(this);
     }
 
     /**
@@ -77,34 +44,9 @@ public class Bluetooth4Service extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(this.getClass().getSimpleName(), "onStartCommand");
-        startAdvertiser();
+        bt4Layer.startAdvertising();
 
         return mStartMode;
-    }
-
-    public void startAdvertiser() {
-        AdvertiseSettings.Builder advertiseSettingsBuilder = new AdvertiseSettings.Builder();
-        advertiseSettingsBuilder
-                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-                .setConnectable(true);
-        AdvertiseSettings advertiseSettings = advertiseSettingsBuilder.build();
-
-        String[] loads = {"happen", "foobar", "lekker", "service", "matetee"};
-        int index = new Random().nextInt(loads.length);
-        byte[] payload = loads[index].getBytes();
-        AdvertiseData.Builder advertiseDataBuilder = new AdvertiseData.Builder();
-        advertiseDataBuilder
-                .addServiceData(parcelUuid, payload)
-                .setIncludeDeviceName(true)
-                .setIncludeTxPowerLevel(true);
-        AdvertiseData advertiseData = advertiseDataBuilder.build();
-
-        //TODO start once only
-        mBluetoothLeAdvertiser.startAdvertising(advertiseSettings, advertiseData, mAdvertiseCallback);
-
-        getRandomNumber();
-        Log.d("random number", "" + number);
     }
 
     /**
@@ -130,7 +72,7 @@ public class Bluetooth4Service extends Service {
     @Override
     public void onDestroy() {
         Log.d(this.getClass().getSimpleName(), "onDestroy");
-        mBluetoothLeAdvertiser.stopAdvertising(mAdvertiseCallback);
+        bt4Layer.stopAdvertising();
     }
 
     /**
@@ -142,19 +84,9 @@ public class Bluetooth4Service extends Service {
         return mBinder;
     }
 
-    /**
-     * method for clients
-     */
-    public int getRandomNumber() {
-        Log.d("CALL", "getRandomNumber");
-        if (number == -1) {
-            number = mGenerator.nextInt(100);
-        }
-        return number;
-    }
-
     @Override
     public void onTaskRemoved(Intent rootIntent) {
+        Log.d(this.getClass().getSimpleName(), "onTaskRemoved");
         super.onTaskRemoved(rootIntent);
     }
 
