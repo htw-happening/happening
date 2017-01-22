dcd9f539073d91994822d83fe4731b26840388e7
Kai Schulz <mail@kaischulz.com>
2017-01-22 13:06:54 +0100

    introduce happening aidl lib & happening aidl service

---
+++ happening_aidl_service/src/main/java/com/happening/poc/happening_aidl_service/HappeningService.java
@@ -0,0 +1,12 @@
+package com.happening.poc.happening_aidl_service;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HappeningService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
