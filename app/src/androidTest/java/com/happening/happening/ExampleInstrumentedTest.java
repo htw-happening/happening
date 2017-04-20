cf28632820e7cb526b6ad7017adbce5e50e7c196
Kai Schulz <mail@kaischulz.com>
2017-04-20 13:27:27 +0200

    fix domain names in junit tests

--- app/src/androidTest/java/com/happening/happening/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/happening/happening/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.happening.poc.poc_happening", appContext.getPackageName());
+        assertEquals("com.happening.poc_happening", appContext.getPackageName());
     }
 }
--- sdk/src/androidTest/java/com/happening/sdk/ExampleInstrumentedTest.java
+++ sdk/src/androidTest/java/com/happening/sdk/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.happening.poc5.sdk.test", appContext.getPackageName());
+        assertEquals("com.happening.sdk.test", appContext.getPackageName());
     }
 }
