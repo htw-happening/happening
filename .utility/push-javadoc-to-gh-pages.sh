422bfc805bc34708709db7571333a7711c08d061
Kai Schulz <mail@kaischulz.com>
2017-04-25 23:32:42 +0200

    add javadoc upload to gh-pages with travis

---
+++ .utility/push-javadoc-to-gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "htw-happening/AndroidApp" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R build/docs/javadoc $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "blue@happening.blue"
+  git config --global user.name "happening-blue"
+  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/htw-happening/AndroidApp master > /dev/null
+
+  cd master
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to master"
+  git push -fq origin master > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
+  
+fi
