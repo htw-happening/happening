16d6236e093156d92193f8c11e33c5acb77a4a18
Nicolas Drebenstedt <cutoffthetop@me.com>
2017-05-16 14:13:25 +0200

    Sort out permission requests

--- service/src/main/java/blue/happening/service/MainActivity.java
+++ service/src/main/java/blue/happening/service/MainActivity.java
@@ -1,8 +1,14 @@
 package blue.happening.service;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Translucent {@link Activity activity} that finishes shortly after creation
@@ -10,11 +16,32 @@ import android.util.Log;
  */
 public class MainActivity extends Activity {
 
+    private static final int TAG_MULTI_PERMISSION_REQUESTS = 100;
+    private static final String[] REQUIRED_PERMISSIONS = new String[]{
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.RECEIVE_BOOT_COMPLETED};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.v(this.getClass().getSimpleName(), "onCreate");
-        // TODO: Verify compatibility and prompt for permissions.
+
+        List<String> requiredPermissions = new ArrayList<>();
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+                requiredPermissions.add(permission);
+            }
+        }
+        if (requiredPermissions.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    requiredPermissions.toArray(new String[0]),
+                    TAG_MULTI_PERMISSION_REQUESTS);
+        }
+
         finish();
     }
 
