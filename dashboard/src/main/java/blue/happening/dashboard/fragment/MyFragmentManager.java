e9034b089f93dfe22f2d2bfde20457e734b6b745
Kai Schulz <mail@kaischulz.com>
2017-05-31 12:41:15 +0200

    extract MenuItems to extra class

--- dashboard/src/main/java/blue/happening/dashboard/fragment/MyFragmentManager.java
+++ dashboard/src/main/java/blue/happening/dashboard/fragment/MyFragmentManager.java
@@ -5,8 +5,6 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.util.Log;
 
-import java.util.LinkedList;
-
 import blue.happening.dashboard.R;
 
 public class MyFragmentManager {
@@ -93,57 +91,6 @@ public class MyFragmentManager {
                 .replace(current.getId(), fragment, tag)
                 .addToBackStack(null)
                 .commit();
-
-    }
-
-    public enum MenuItems {
-        /**
-         * List of Enums
-         * Add a new Enum to create a new Menu item
-         */
-        DASHBOARD_FRAGMENT("dashboard"),
-        IMPRESSUM_FRAGMENT("impressum");
-
-        // fields
-        private final String name;
-
-        // constructor
-        MenuItems(final String name) {
-            this.name = name;
-        }
-
-        /**
-         * get Enum Object by name
-         *
-         * @param name
-         * @return
-         */
-        public static MenuItems getById(String name) {
-            for (MenuItems e : MenuItems.values()) {
-                if (e.name == name) {
-                    return e;
-                }
-            }
-            return null;// not found
-        }
-
-        /**
-         * return all enums names as string array
-         *
-         * @return
-         */
-        public static String[] toArray() {
-            LinkedList<String> list = new LinkedList<>();
-            for (MenuItems s : MenuItems.values()) {
-                list.add(s.getName());
-            }
-
-            return list.toArray(new String[list.size()]);
-        }
-
-        public String getName() {
-            return name;
-        }
 
     }
 
