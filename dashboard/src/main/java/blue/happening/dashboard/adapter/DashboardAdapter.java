8233fb28cc6f77b01716ece421fdb6f741bdae2f
fabiberlin <fabi.in.berlin@googlemail.com>
2017-06-13 19:48:57 +0200

    some send and receive

--- dashboard/src/main/java/blue/happening/dashboard/adapter/DashboardAdapter.java
+++ dashboard/src/main/java/blue/happening/dashboard/adapter/DashboardAdapter.java
@@ -2,15 +2,18 @@ package blue.happening.dashboard.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import blue.happening.dashboard.R;
+import blue.happening.dashboard.fragment.DashboardFragment;
 import blue.happening.dashboard.model.DashboardModel;
 
 
@@ -22,7 +25,7 @@ public class DashboardAdapter extends ArrayAdapter<DashboardModel> {
 
     @NonNull
     @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, View convertView, @NonNull ViewGroup parent) {
 
         DashboardModel dashboardModel = getItem(position);
 
@@ -37,6 +40,17 @@ public class DashboardAdapter extends ArrayAdapter<DashboardModel> {
             title.setText(dashboardModel.getTitle());
             message.setText(dashboardModel.getMessage());
         }
+
+        convertView.findViewById(R.id.dashboard_button_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Send", Toast.LENGTH_SHORT).show();
+                String message = "Hey there, i'm using happening!";
+                DashboardModel model = getItem(position);
+                Log.d(getClass().getSimpleName(), "Sending Message to " + model.getMessage());
+                DashboardFragment.getInstance().getHappening().sendDataTo(model.getMessage(), message.getBytes());
+            }
+        });
 
         return convertView;
     }
