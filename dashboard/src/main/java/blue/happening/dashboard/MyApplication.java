1bf9c8c23758b72a496dad314e3db2c14c30eb87
Kai Schulz <mail@kaischulz.com>
2017-07-04 12:35:32 +0200

    add acra error log to dashboard

--- dashboard/src/main/java/blue/happening/dashboard/MyApplication.java
+++ dashboard/src/main/java/blue/happening/dashboard/MyApplication.java
@@ -3,6 +3,13 @@ package blue.happening.dashboard;
 import android.app.Application;
 import android.content.Context;
 
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(
+        formUri = "http://collector.tracepot.com/6ed46252",
+        logcatArguments = { "-t", "100", "-v", "long", "ActivityManager:I", "MyApp:D", "*:S" }
+)
 public class MyApplication extends Application {
 
     private static Context context;
@@ -15,6 +22,14 @@ public class MyApplication extends Application {
 
     public static Context getAppContext() {
         return MyApplication.context;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
     }
 
 }
