3e7e4c73ad438e52514ca37cc969a8f1a3c85bc4
Nicolas Drebenstedt <cutoffthetop@me.com>
2017-07-05 15:12:35 +0200

    Oops

--- mesh/src/main/java/blue/happening/mesh/RoutingTable.java
+++ mesh/src/main/java/blue/happening/mesh/RoutingTable.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 
-class RoutingTable extends ConcurrentHashMap<String, RemoteDevice> {
+public class RoutingTable extends ConcurrentHashMap<String, RemoteDevice> {
 
     private IMeshHandlerCallback meshHandlerCallback;
 
