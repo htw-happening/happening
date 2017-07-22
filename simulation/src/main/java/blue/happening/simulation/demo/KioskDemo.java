32f602afb0e57093740aa68bcfe3a06c0de15c8e
Nicolas Drebenstedt <cutoffthetop@me.com>
2017-07-22 15:27:14 +0200

    Configure route creation experiments

--- simulation/src/main/java/blue/happening/simulation/demo/KioskDemo.java
+++ simulation/src/main/java/blue/happening/simulation/demo/KioskDemo.java
@@ -1,8 +1,10 @@
 package blue.happening.simulation.demo;
 
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import blue.happening.simulation.entities.Connection;
 import blue.happening.simulation.entities.Device;
@@ -27,11 +29,6 @@ public class KioskDemo extends HappeningDemo {
 
         class MobilityFactory<V, E> {
             private List<MobilityPattern<V, E>> patterns = new ArrayList<>();
-            private boolean onRepeat;
-
-            public MobilityFactory(boolean onRepeat) {
-                this.onRepeat = onRepeat;
-            }
 
             private MobilityFactory<V, E> addWaypoints(String... waypoints) {
                 List<Waypoint<V, E>> waypointList = new ArrayList<>();
@@ -44,31 +41,57 @@ public class KioskDemo extends HappeningDemo {
                         waypointList.add(new DTWaypoint<V, E>(sxf, syf, travelTime));
                     }
                 }
-                patterns.add(new PredefinedMobilityPattern<>(onRepeat, waypointList));
+                patterns.add(new PredefinedMobilityPattern<>(false, waypointList));
                 return this;
             }
 
             private List<MobilityPattern<V, E>> getPatterns() {
-                return patterns;
+                List<MobilityPattern<V, E>> copy = new ArrayList<>(patterns);
+                patterns.clear();
+                return copy;
             }
         }
 
-        List<MobilityPattern<Device, Connection>> newNeighbour = new MobilityFactory<Device, Connection>(false)
+        MobilityFactory<Device, Connection> factory = new MobilityFactory<>();
+        HashMap<String, List<MobilityPattern<Device, Connection>>> patterns = new HashMap<>();
+
+        patterns.put("newNeighbour", factory
                 .addWaypoints(" 0, 0, 0")
-                .addWaypoints("50, 0, 0", "20, 0, 30")
-                .getPatterns();
+                .addWaypoints("50, 0, 0", "50, 0, 10", "20, 0, 20")
+                .getPatterns());
 
+        patterns.put("newMultihop", factory
+                .addWaypoints("10, 0, 0")
+                .addWaypoints("30, 0, 0")
+                .addWaypoints("90, 0, 0", "90, 0, 10", "50, 0, 20")
+                .getPatterns());
 
-        List<MobilityPattern<Device, Connection>> newRemote = new MobilityFactory<Device, Connection>(false)
-                .addWaypoints(" 0, 0, 0")
-                .addWaypoints("20, 0, 0")
-                .addWaypoints("80, 0, 0", "40, 0, 30")
-                .getPatterns();
+        patterns.put("alsoNeighbour", factory
+                .addWaypoints("35,  5, 0")
+                .addWaypoints("50, 10, 0")
+                .addWaypoints("75, 10, 0", "75, 10, 10", "50, 0, 5")
+                .getPatterns());
 
-        List<MobilityPattern<Device, Connection>> patterns = newRemote;
-        for (int i = 0; i < patterns.size(); i++) {
+        patterns.put("alsoMultihop", factory
+                .addWaypoints(" 0, 10, 0")
+                .addWaypoints("10,  0, 0", "10,  0, 10", "15,  5, 10")
+                .addWaypoints("10, 20, 0", "10, 20, 10", "15, 15, 10")
+                .getPatterns());
+
+        patterns.put("newRoute", factory
+                .addWaypoints(" 0, 10, 0")
+                .addWaypoints("15,  2, 0")
+                .addWaypoints("15, 18, 0")
+                .addWaypoints("35, 18, 0", "35, 18, 10", "30, 10, 10")
+                .getPatterns());
+
+        String[] choices = patterns.keySet().toArray(new String[patterns.size()]);
+        String selection = (String) JOptionPane.showInputDialog(null, "Choose now...",
+                "Select kiosk demo", JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+
+        for (int i = 0; i < patterns.get(selection).size(); i++) {
             Device device = new Device("Device_" + i, getGraph(), postman, runner, messageDelay, messageLoss);
-            MobilityPattern<Device, Connection> pattern = patterns.get(i);
+            MobilityPattern<Device, Connection> pattern = patterns.get(selection).get(i);
             DTWaypoint<Device, Connection> initalWaypoint = (DTWaypoint<Device, Connection>) pattern.nextWaypoint(getGraph(), device);
             getGraph().addVertex(device, initalWaypoint.getSxf(), initalWaypoint.getSyf(), pattern, txRadius, rxRadius);
         }
