b5070a2c4ce8625016d56eeedf2080273ffd0e6d
fabiberlin <fabi.in.berlin@googlemail.com>
2017-07-25 21:36:13 +0200

    update javadoc

--- sdk/src/main/java/blue/happening/sdk/HappeningCallback.java
+++ sdk/src/main/java/blue/happening/sdk/HappeningCallback.java
@@ -1,7 +1,13 @@
 package blue.happening.sdk;
 
+import android.content.Context;
+
 import blue.happening.HappeningClient;
 
+/**
+ * Implement these methods for getting callbacks of the happening network. Mandatory for
+ * {@link Happening#register(Context, HappeningCallback)}.
+ */
 public interface HappeningCallback {
 
     int MESSAGE_ACTION_ARRIVED = 0;
@@ -13,9 +19,38 @@ public interface HappeningCallback {
     int MESSAGE_TYPE_OGM = 1;
     int MESSAGE_TYPE_UCM = 2;
 
+    /**
+     * Callback if a new network {@link HappeningClient client} appeared.
+     * @param client
+     */
     void onClientAdded(HappeningClient client);
+
+    /**
+     * Callback if a network {@link HappeningClient client} was updated.
+     * @param client
+     */
     void onClientUpdated(HappeningClient client);
+
+    /**
+     * Callback if a network {@link HappeningClient client} was removed.
+     * @param client
+     */
     void onClientRemoved(HappeningClient client);
+
+    /**
+     * Callback for showing the network traffic for this device.
+     * @param packageType {@link #MESSAGE_TYPE_OGM}, {@link #MESSAGE_TYPE_UCM}
+     * @param action {@link #MESSAGE_ACTION_ARRIVED}, {@link #MESSAGE_ACTION_DROPPED},
+     *                                              {@link #MESSAGE_ACTION_FORWARDED},
+     *                                              {@link #MESSAGE_ACTION_RECEIVED},
+     *                                              {@link #MESSAGE_ACTION_SENT}
+     */
     void onMessageLogged(int packageType, int action);
+
+    /**
+     * Callback if the app receives a message from another {@link HappeningClient}.
+     * @param message
+     * @param source
+     */
     void onMessageReceived(byte[] message, HappeningClient source);
 }
